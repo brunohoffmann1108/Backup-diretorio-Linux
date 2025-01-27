75de8fc25015866c6776cab0b08cdb14d4df5b79
Bruno <brunohoffmann.2003@gmail.com>
2025-01-27 10:16:18 -0300

    Adiciona o monitoramento de uso de disco

--- monitoramento-recursos/monitoramento-recursos.sh
+++ monitoramento-recursos/monitoramento-recursos.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+#Uso de CPU em porcentagem.
 usoCPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+
+#Uso de memória em porcentagem.
 usoMemoria=$(free -m | awk 'NR==2{printf "%.2f\n", $3*100/$2}')
+
+#Uso de disco em porcentagem.
+usoDisco=$(df -h | awk '$NF=="/"{print $5}')
 
 echo "Uso de CPU: "$usoCPU"%"
 echo "Uso de memória RAM: "$usoMemoria"%"
+echo "Uso de disco: "$usoDisco""
