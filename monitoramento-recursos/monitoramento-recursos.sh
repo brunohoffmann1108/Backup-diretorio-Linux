374a03d0503f2f462cea9ab9d1afac2d719a41a3
Bruno <brunohoffmann.2003@gmail.com>
2025-01-27 11:30:48 -0300

    Adiciona o monitoramento dos 5 processos mais pesados e printa tudo em um arquivo de log localizado em uma pasta especifica, ou seja, não pode ser executado genericamente

--- monitoramento-recursos/monitoramento-recursos.sh
+++ monitoramento-recursos/monitoramento-recursos.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+#Data atual.
+dataAtual=$(date +"%d-%m-%Y_%H:%M:%S")
+
+#Arquivo LOG.
+LOG=~/recursos/"$dataAtual".log
+
 #Uso de CPU em porcentagem.
 usoCPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+
+#Processos mais pesados.
+processos=$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6)
 
 #Uso de memória em porcentagem.
 usoMemoria=$(free -m | awk 'NR==2{printf "%.2f\n", $3*100/$2}')
@@ -9,6 +18,9 @@ usoMemoria=$(free -m | awk 'NR==2{printf "%.2f\n", $3*100/$2}')
 #Uso de disco em porcentagem.
 usoDisco=$(df -h | awk '$NF=="/"{print $5}')
 
-echo "Uso de CPU: "$usoCPU"%"
-echo "Uso de memória RAM: "$usoMemoria"%"
-echo "Uso de disco: "$usoDisco""
+printf "MONITORAMENTO DE RECURSOS %s\n" "$dataAtual" >> "$LOG"
+printf "Uso de CPU: %s\n" "$usoCPU%" >> "$LOG"
+printf "5 Processos mais pesados:\n%s\n" "$processos" >> "$LOG"
+printf "Memória RAM em uso: %s%%\n" "$usoMemoria" >> "$LOG"
+printf "Uso de disco: %s\n" "$usoDisco" >> "$LOG"
+
