233222776d9f83d4f8cd21abe8934d407cbffb84
Bruno <brunohoffmann.2003@gmail.com>
2025-01-28 11:25:53 -0300

    Adiciona quantidades em GB para memória e disco e também adiciona a funcionalidade de monitoramento contínuo

--- monitoramento-recursos/monitoramento-recursos.sh
+++ monitoramento-recursos/monitoramento-recursos.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-#Data atual.
-dataAtual=$(date +"%d-%m-%Y_%H:%M:%S")
+while true; do
+    # CPU:
+    usoCPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+    processos=$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6)
 
-#Uso de CPU em porcentagem.
-usoCPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+    # Memória RAM:
+    usoMemoria=$(free -m | awk 'NR==2{printf "%.2f\n", $3*100/$2}')
+    usoMemoriaGB=$(free -m | awk 'NR==2{printf "%.2f\n", $3/1000}')
+    memoriaTotalGB=$(free -m | awk 'NR==2{printf "%.2f\n", $2/1000}')
 
-#Processos mais pesados.
-processos=$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6)
+    # Disco:
+    usoDisco=$(df -h | awk '$NF=="/"{print $5}')
+    usoDiscoGB=$(df -h | awk '$NF=="/"{print $3}')
+    discoTotalGB=$(df -h | awk '$NF=="/"{print $2}')
 
-#Uso de memória em porcentagem.
-usoMemoria=$(free -m | awk 'NR==2{printf "%.2f\n", $3*100/$2}')
 
-#Uso de disco em porcentagem.
-usoDisco=$(df -h | awk '$NF=="/"{print $5}')
-
-printf "MONITORAMENTO DE RECURSOS %s\n" "$dataAtual"
-printf "Uso de CPU: %s\n" "$usoCPU%"
-printf "5 Processos mais pesados:\n%s\n" "$processos"
-printf "Memória RAM em uso: %s%%\n" "$usoMemoria"
-printf "Uso de disco: %s\n" "$usoDisco"
+    # Informações formatadas:
+    clear
+    printf "\e[1;31m5 Processos mais pesados:\e[0m\n%s\n" "$processos"
+    printf "\e[1;31mUso de CPU:\e[0m %s\n" "$usoCPU%"
+    printf "\e[1;31mMemória RAM em uso:\e[0m %s%% ("$usoMemoriaGB"GB de "$memoriaTotalGB"GB)\n" "$usoMemoria"
+    printf "\e[1;31mUso de disco:\e[0m %s\n" "$usoDisco ("$usoDiscoGB"B de "$discoTotalGB"B)"
+    sleep 1
+done
 
