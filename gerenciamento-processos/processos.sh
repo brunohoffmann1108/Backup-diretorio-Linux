1ab1eedf575be37559732a2435a781b631a4c6b4
Bruno <brunohoffmann.2003@gmail.com>
2025-02-04 17:04:10 -0300

    Finaliza projeto de gerenciamento de processos

--- gerenciamento-processos/processos.sh
+++ gerenciamento-processos/processos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
+if [[ $# != 2 ]]; then
     echo -e "\e[1;31mUso correto:\e[0m $(basename "$0") <opção> <argumento_opção>"
+    exit 1 
 fi
 
 OPCAO=$1
@@ -9,20 +10,30 @@ ARGUMENTO_OPCAO=$2
 
 case "$OPCAO" in
     "listar")
-
+        if [[ "$ARGUMENTO_OPCAO" =~ ^[0-9]+$ ]]; then
+            ((ARGUMENTO_OPCAO++))
+            ps -eo user,pid,%cpu,cmd --sort=-%cpu | awk '{print $1, $2, $3, $4}' | column -t | head -n $ARGUMENTO_OPCAO
+        else
+            echo -e "\e[1;31mUso correto:\e[0m $(basename "$0") listar <número_processos>"
+            echo -e "Segundo argumento deve ser um número inteiro positivo"
+        fi
     ;;
     "nome")
-
-    ;;
-    "recursos")
-
+        RETORNO=$(ps -eo user,pid,%cpu,cmd | grep -i "$ARGUMENTO_OPCAO\|%cpu" | grep -v "grep" | grep -v "$(basename "$0")" | awk '{print $1, $2, $3, $4}' | column -t)
+        if [[ -z "$RETORNO" ]]; then
+            echo "Sem correspondências."
+        else
+            echo "$RETORNO"
+        fi
     ;;
     "matar")
-
+        kill "$ARGUMENTO_OPCAO" && echo "Processo finalizado." || echo "Erro ao tentar finalizar o processo."
     ;;
     *)
         echo -e "\e[1;31mOpção inválida.\e[0m"
         exit 1
     ;;
 esac
+
+
 
